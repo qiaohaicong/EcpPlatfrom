ab83f130b1f82032a350728306c6097bd3a35502
qhc <qiaohaicong@163.com>
2020-09-24 15:48:27 +0800

    环境搭建测试通了

---
+++ ecp-server/fee-server/src/main/java/com/feeserver/EcpFeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.feeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcpFeeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcpFeeServerApplication.class, args);
+    }
+
+}
